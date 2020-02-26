4fc39588fd7c9fcce2167037a6be55f089a87afc
arparker5 <arparker5@alaska.edu>
2020-02-26 12:03:12 -0900

    Slowly drops variables to find what data is valuable

--- regression/LEregression.py
+++ regression/LEregression.py
@@ -19,16 +19,13 @@ def myRegression(X_columns, Y):
     Y_pred = reg.predict(X_test)
     df = pd.DataFrame({'Actual': Y_test, 'Predicted': Y_pred})
 
-    print("Regression model output (first 25 variables): ")
-    print()
-    print(df.head(25))
+    # print("Regression model output(first 25 variables): ")
+    # print()
+    # print(df.head(25))
 
     print('Mean Absolute Error:', metrics.mean_absolute_error(Y_test, Y_pred))
     print('Mean Squared Error:', metrics.mean_squared_error(Y_test, Y_pred))
     print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(Y_test, Y_pred)))
-
-
-
 
 
 data = pd.read_csv('../Life Expectancy Data.csv')
@@ -49,9 +46,35 @@ X_columns = ['Status', 'Adult Mortality', 'infant deaths',
 
 Y = data['Life expectancy '].values
 
+print()
+print("------------ Model with all variables ------------")
 myRegression(X_columns, Y)
 
-# ----------------- Begin second model ----------------- #
+# ----------------- Dropping 'Population' ----------------- #
+
+X_columns = ['Status', 'Adult Mortality', 'infant deaths',
+          'Alcohol', 'percentage expenditure', 'Hepatitis B', 'Measles ',
+          ' BMI ', 'under-five deaths ', 'Polio', 'Total expenditure', 'Diphtheria ',
+          ' HIV/AIDS', 'GDP', ' thinness  1-19 years', ' thinness 5-9 years',
+          'Income composition of resources', 'Schooling']
+
+print()
+print("------------ Model with 'population' dropped ------------")
+myRegression(X_columns, Y)
+
+# ----------------- Dropping Measles, GDP ----------------- #
+
+X_columns = ['Status', 'Adult Mortality', 'infant deaths',
+          'Alcohol', 'percentage expenditure', 'Hepatitis B',
+          ' BMI ', 'under-five deaths ', 'Polio', 'Total expenditure', 'Diphtheria ',
+          ' HIV/AIDS', ' thinness  1-19 years', ' thinness 5-9 years',
+          'Income composition of resources', 'Schooling']
+
+print()
+print("------------ Dropping Measles, GDP ------------")
+myRegression(X_columns, Y)
+
+# ----------------- Dropping 'percentage expenditure' ----------------- #
 
 X_columns = ['Status', 'Adult Mortality', 'infant deaths',
           'Alcohol', 'Hepatitis B',
@@ -59,8 +82,9 @@ X_columns = ['Status', 'Adult Mortality', 'infant deaths',
           ' HIV/AIDS', ' thinness  1-19 years', ' thinness 5-9 years',
           'Income composition of resources', 'Schooling']
 
-X = data[X_columns].values
-
+print()
+print("------------ Dropping 'percentage expenditure' ------------")
+myRegression(X_columns, Y)
 
 # plt.figure(figsize=(15, 10))
 # plt.tight_layout()
