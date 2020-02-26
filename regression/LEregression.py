aead5a8a19dddd3d71bafa5239f3a6ea0ef02b9a
arparker5 <arparker5@alaska.edu>
2020-02-25 16:33:24 -0900

    Runs regression with all variables, finds regression coefficients

--- regression/LEregression.py
+++ regression/LEregression.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn import metrics
@@ -7,7 +8,7 @@ from sklearn import metrics
 
 data = pd.read_csv('../Life Expectancy Data.csv')
 print(data.shape)
-print(data.describe())
+# print(data.describe())
 data = data.fillna(method='bfill')
 
 d = {'Developing': 0, 'Developed': 1}
@@ -17,4 +18,30 @@ data['Status'] = data['Status'].map(d).fillna(data['Status'])
 # print(data.describe())
 
 
-# X = data[[]]
+X = data[['Status', 'Adult Mortality', 'infant deaths',
+          'Alcohol', 'percentage expenditure', 'Hepatitis B', 'Measles ',
+          ' BMI ', 'under-five deaths ', 'Polio', 'Total expenditure', 'Diphtheria ',
+          ' HIV/AIDS', 'GDP', 'Population', ' thinness  1-19 years', ' thinness 5-9 years',
+          'Income composition of resources', 'Schooling']].values
+
+X_columns = ['Status', 'Adult Mortality', 'infant deaths',
+          'Alcohol', 'percentage expenditure', 'Hepatitis B', 'Measles ',
+          ' BMI ', 'under-five deaths ', 'Polio', 'Total expenditure', 'Diphtheria ',
+          ' HIV/AIDS', 'GDP', 'Population', ' thinness  1-19 years', ' thinness 5-9 years',
+          'Income composition of resources', 'Schooling']
+
+Y = data['Life expectancy '].values
+
+plt.figure(figsize=(15, 10))
+plt.tight_layout()
+sns.distplot(data['Life expectancy '])
+# plt.show()
+
+# 80/20 split of training and test data
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
+
+reg = LinearRegression()
+reg.fit(X_train, Y_train)
+
+coeff_df = pd.DataFrame(reg.coef_, X_columns, columns=['Coefficient'])
+print(coeff_df)
